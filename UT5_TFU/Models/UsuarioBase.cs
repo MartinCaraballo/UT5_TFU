7eca3bd9cfcc247d0bc6689d1f8e3d6ca3e69f0e
Juan Riccetto <juanmartinre@gmail.com>
2024-06-19 18:19:14 -0300

    UsuarioBase

--- UT5_TFU/Models/UsuarioBase.cs
+++ UT5_TFU/Models/UsuarioBase.cs
@@ -1,6 +1,6 @@
 namespace WebApp.Models
 {
-    public abstract class Usuario
+    public class UsuarioBase : Usuario
     {
         public int Id { get; set; }
         public string Nombre { get; set; }
